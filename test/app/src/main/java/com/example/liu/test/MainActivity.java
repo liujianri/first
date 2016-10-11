185902ff51afe5bc977b33ac43db697fa87023d8
liujianri <liu@macdeMBP-2.lan>
2016-10-11 13:23:22 +0800

    frist
    one

---
+++ test/app/src/main/java/com/example/liu/test/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.liu.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                execShell("am start -n com.hellotalk/.ui.main.MainTabActivity");
+            }
+        });
+
+    }
+
+
+    public void execShell(String cmd){
+        try{
+            //权限设置
+            Process p = Runtime.getRuntime().exec("su");
+            //获取输出流
+            OutputStream outputStream = p.getOutputStream();
+            DataOutputStream dataOutputStream=new DataOutputStream(outputStream);
+            //将命令写入
+            dataOutputStream.writeBytes(cmd);
+            //提交命令
+            dataOutputStream.flush();
+            //关闭流操作
+            dataOutputStream.close();
+            outputStream.close();
+        }
+        catch(Throwable t)
+        {
+            t.printStackTrace();
+        }
+    }
+}
